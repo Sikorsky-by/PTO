c97f6cf7597b2b01396f8a28fa0f96ade49d969e
Sikorsky-by <chebanov33@gmail.com>
2022-08-05 16:00:59 +0300

    work work work

--- PTO/PTOfunctionCalls.cs
+++ PTO/PTOfunctionCalls.cs
@@ -17,7 +17,7 @@ namespace PTO
         public static double ТНАСЫЩЕНИЯ([ExcelArgument(Name = "Pизб", Description = "Избыточное давление, bar")] double Pa,
                                         [ExcelArgument(Name = "Pатм", Description = "Атмосферное давление, bar\nЕсли не указано применяется 1,01325 bar")] double Patm)
         {
-            double Press = 0;
+            double Press;
 
             if (Patm == 0)
                 Press = Pa + 1.01325;
@@ -49,10 +49,12 @@ namespace PTO
                                         [ExcelArgument(Name = "Pизб", Description = "Избыточное давление, bar")] double Pa,
                                         [ExcelArgument(Name = "Pатм", Description = "Атмосферное давление, bar\nЕсли не указано применяется 1,01325 bar")] double Patm)
         {
-            double Press = 0;
+            double Press;
 
             if (T < 0)
-                throw new ArgumentOutOfRangeException("Температура", T, "Температура меньше 0");
+                throw new ArgumentOutOfRangeException("Температура", T, "Температура меньше 0 °С");
+            if (T > 800)
+                throw new ArgumentOutOfRangeException("Температура", T, "Температура больше 800 °С");
             if (Patm == 0)
                 Press = Pa + 1.01325;
             else
@@ -62,9 +64,19 @@ namespace PTO
 
             double Tn = ТНАСЫЩЕНИЯ(Pa, Patm);
 
+            int index = EntalpyPressureList.FindIndex(x => x >= Press) - 1;
+            KeyValuePair<double, List<double>> e2 = Entalpy.First(x => x.Key > T);
+            KeyValuePair<double, List<double>> e1 = Entalpy.First(x => x.Key == e2.Key - 10.0);
 
+            if (e1.Key > Tn && Tn > e2.Key)
+            {
+                // точка насышения
+            }
 
-            return 0;
+            double ent1 = e1.Value[index] + (e2.Value[index] - e1.Value[index]) / (e2.Key - e1.Key) * (T - e1.Key);
+            double ent2 = e1.Value[index + 1] + (e2.Value[index + 1] - e1.Value[index + 1]) / (e2.Key - e1.Key) * (T - e1.Key);
+
+            return ent1 + (ent2 - ent1) / (EntalpyPressureList[index + 1] - EntalpyPressureList[index]) * (Press - EntalpyPressureList[index]);
         }
     }
 }
